e9ab3bab32b409b5fd21807775babe260b7593b1
Olga Thompson <olgatj2007@gmail.com>
2021-01-22 17:18:21 -0500

    redux initial setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,6 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', (req, res) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('hello world!');
-});
 
 
 module.exports = router;
