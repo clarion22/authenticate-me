bb1678c983969cebcec46067c0569793904842a8
Olga Thompson <olgatj2007@gmail.com>
2021-01-22 16:10:14 -0500

    auth middlware complete

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -20,4 +20,40 @@ const setTokenCookie = (res, user) => {
     sameSite: isProduction && "Lax",
   });
   return token;
-}
+};
+
+const restoreUser = (req, res, next) => {
+  const { token } = req.cookies;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
+      return next();
+    }
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope('currentUser').findByPk(id);
+    } catch (e) {
+      res.clearCookie('token');
+      return next();
+    }
+
+    if (!res.user) res.clearCookie('token');
+
+    return next();
+  });
+};
+
+const requireAuth = [
+  restoreUser,
+  function (req, res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Unauthorized');
+    err.title = 'Unauthorized';
+    err.errors = ['Unauthorized'];
+    err.status = 401;
+    return next(err);
+  },
+];
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
