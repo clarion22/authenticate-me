e5a78871ac666262d13df6cd3fe9f5985fd429d9
Olga Thompson <olgatj2007@gmail.com>
2021-01-22 16:31:00 -0500

    login auth route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
 
 module.exports = router;
