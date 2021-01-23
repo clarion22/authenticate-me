f9ded7857b96f46e9f7a4a94199c3efec14e31d8
Olga Thompson <olgatj2007@gmail.com>
2021-01-23 17:56:09 -0500

    login modal

--- frontend/src/context/Modal.js
+++ frontend/src/context/Modal.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.css';
 
